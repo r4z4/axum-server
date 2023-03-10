641b8851cdc38bb72815c2994a5547fcbde041c3
r4z4 <r4z4aa@gmail.com>
2023-03-09 19:05:04 -0600

    post req

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,13 +1,17 @@
 mod hello_world;
+mod mirror_body_string;
 
 use axum::{
-    routing::{get, post, put},
+    routing::{get, post},
     Router, body::Body,
 };
 
 use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router<(), Body> {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
 
