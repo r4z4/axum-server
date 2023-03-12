076733cfe75e42f6499e4c336e3e98378ea39797
r4z4 <r4z4aa@gmail.com>
2023-03-11 23:01:06 -0600

    db

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod routes;
 use routes::create_routes;
+use sea_orm::Database;
 
-pub async fn run() {
+pub async fn run(database_uri: &str) {
+    let database = Database::connect(database_uri).await;
     let app = create_routes();
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use axum_server::run;
+use dotenvy::dotenv;
+use dotenvy_macro::dotenv;
 
 #[tokio::main]
 async fn main() {
-    run().await
+    dotenv().ok();
+    let database_uri = dotenv!("DATABASE_URL");
+    run(database_uri).await
 }
 
+
+/*
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+*/
